0a3061957f18bae0668dce9f563e3a809b5354f2
EsmaeeilMoradi <esmaeeilmoradi700@gmail.com>
2023-08-09 18:04:06 +0330

    Enabling ViewBinding in Android.

--- app/src/main/java/com/esm/viewbinding/MainActivity.kt
+++ app/src/main/java/com/esm/viewbinding/MainActivity.kt
@@ -2,10 +2,30 @@ package com.esm.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.esm.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+//        val myTextView : TextView = findViewById<TextView>(R.id.myTextView)
+//        myTextView.text = "This is a test"
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val myBank :Int = 1000
+
+        if (binding.dollarValue.text.isNotEmpty()) {
+            val dollarValue = binding.dollarValue.text.toString().toFloat()
+
+            val euroValue = myBank * 0.92f
+            binding.euroValue.text = euroValue.toString()
+        }else{
+            binding.euroValue.text=""
+        }
+
     }
 }
